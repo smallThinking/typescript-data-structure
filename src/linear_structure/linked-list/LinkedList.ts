9703770a8b77b1240256f0fb45293093f6728630
smallThinking <wnsdnek778@gmail.com>
2018-11-22 23:23:49 +0900

    hash table implements

--- src/linear_structure/linked-list/LinkedList.ts
+++ src/linear_structure/linked-list/LinkedList.ts
@@ -1,5 +1,5 @@
-import {LinkedListNode} from './LinkedListNode';
-import {Comparator} from '../../comparator/Comparator';
+import { LinkedListNode } from "./LinkedListNode";
+import { Comparator } from "../../comparator/Comparator";
 
 export class LinkedList<T> {
   public head: LinkedListNode<T>;
@@ -16,19 +16,17 @@ export class LinkedList<T> {
   //append(뒤에 삽입)
   public append = (data: T): LinkedList<T> => {
     const newNode = new LinkedListNode(data, null);
-    if(!this.head) {
+    if (!this.head) {
       //노드가 없으면?
       this.head = newNode;
       this.tail = newNode;
-    }else {
+    } else {
       //노드가 있으면?
       this.tail.next = newNode;
       this.tail = newNode;
     }
     return this;
-  }
-
-
+  };
 
   //prepend(앞에 삽입)
   public prepend = (data: T): LinkedList<T> => {
@@ -39,35 +37,35 @@ export class LinkedList<T> {
       this.tail = newNode;
     }
     return this;
-  } 
-
-
+  };
 
   //find
-  public find = ({ data = undefined, callback = undefined }): LinkedListNode<T> => {
-    if(!this.head) {
+  public find = ({
+    data = undefined,
+    callback = undefined
+  }): LinkedListNode<T> => {
+    if (!this.head) {
       return null;
     }
 
     let currentNode = this.head;
 
-    while(currentNode){
-      if(callback && callback(currentNode.data)){
+    while (currentNode) {
+      if (callback && callback(currentNode.data)) {
         return currentNode;
       }
-      
-      if(data !== undefined && this.compare.equal(currentNode.data, data)) {
+
+      if (data !== undefined && this.compare.equal(currentNode.data, data)) {
         return currentNode;
       }
       currentNode = currentNode.next;
     }
     return null;
-  }
-
+  };
 
   //Delete
   public delete = (data: T): LinkedListNode<T> => {
-    if(!this.head) {
+    if (!this.head) {
       //비어있는 리스트이면
       return null;
     }
@@ -75,7 +73,7 @@ export class LinkedList<T> {
     let deleteNode = null;
 
     // 지워야 할 것이  처음인 경우
-    if(this.compare.equal(this.head.data, data)){
+    if (this.compare.equal(this.head.data, data)) {
       deleteNode = this.head;
       this.head = this.head.next;
       return deleteNode;
@@ -83,8 +81,8 @@ export class LinkedList<T> {
 
     let currentNode = this.head;
 
-    while(currentNode.next){
-      if(this.compare.equal(currentNode.next.data, data)) {
+    while (currentNode.next) {
+      if (this.compare.equal(currentNode.next.data, data)) {
         deleteNode = currentNode.next;
         currentNode.next = deleteNode.next;
       } else {
@@ -93,36 +91,34 @@ export class LinkedList<T> {
     }
 
     // 지워야 할 것이  마지막인 경우
-    if(this.compare.equal(this.tail.data, data)) {
+    if (this.compare.equal(this.tail.data, data)) {
       this.tail = currentNode;
     }
 
     return deleteNode;
-  }
+  };
 
   public deleteHead = (): LinkedListNode<T> => {
-    if(!this.head){
+    if (!this.head) {
       return null;
     }
-    
+
     let currentNode = this.head;
 
-    if(this.head.next){
+    if (this.head.next) {
       this.head = currentNode.next;
-    }else {
+    } else {
       this.head = null;
       this.tail = null;
     }
 
     return currentNode;
-  }
-
-
+  };
 
   public deleteTail = (): LinkedListNode<T> => {
     const deletedTail = this.tail;
 
-    if(this.head === this.tail){
+    if (this.head === this.tail) {
       this.head = null;
       this.tail = null;
 
@@ -130,10 +126,10 @@ export class LinkedList<T> {
     }
 
     let currentNode = this.head;
-    while(currentNode.next){
-      if(!currentNode.next.next){
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
         currentNode.next = null;
-      } else{
+      } else {
         currentNode = currentNode.next;
       }
     }
@@ -141,22 +137,21 @@ export class LinkedList<T> {
     this.tail = currentNode;
 
     return deletedTail;
-  }
+  };
   //데이터를 array형태로 추출
-  public toArray = ():Array<LinkedListNode<T>> => {
+  public toArray = (): Array<LinkedListNode<T>> => {
     let nodes = [];
 
     let currentNode = this.head;
-    while(currentNode) {
+    while (currentNode) {
       nodes.push(currentNode);
       currentNode = currentNode.next;
     }
 
     return nodes;
-  }
+  };
 
   //Traverse
-
 
   //Traverse in Reverse
 }
--- src/nonlinear_structure/hash-table/HashTable.ts
+++ src/nonlinear_structure/hash-table/HashTable.ts
@@ -1,67 +1,75 @@
-import { LinkedList } from '../../linear_structure/linked-list/LinkedList';
+import { LinkedList } from "../../linear_structure/linked-list/LinkedList";
 
 interface hashNode {
-  key: string,
-  value: any
+  key: string;
+  value: any;
 }
 
 export class HashTable {
-  public buckets:Array<LinkedList<hashNode>>;
+  public buckets: Array<LinkedList<hashNode>>;
   public keys: object;
 
-  constructor(hashTableSize: number = 32){
-    this.buckets = Array(hashTableSize).fill(null).map(() => new LinkedList<hashNode>());
+  constructor(hashTableSize: number = 32) {
+    this.buckets = Array(hashTableSize)
+      .fill(null)
+      .map(() => new LinkedList<hashNode>());
     this.keys = {};
   }
 
-// 문자열의 각 코드 값을 더해 버킷의 길이값 만큼 나누어 인덱스를 반환
-  private hash = (key:string) => {
+  // 문자열의 각 코드 값을 더해 버킷의 길이값 만큼 나누어 인덱스를 반환
+  private hash = (key: string) => {
     const hash = Array.from(key).reduce(
-      (hashAccumulator, keySymbol) => (hashAccumulator + keySymbol.charCodeAt(0)),
-      0,
+      (hashAccumulator, keySymbol) => hashAccumulator + keySymbol.charCodeAt(0),
+      0
     );
 
     return hash % this.buckets.length;
-  }
+  };
 
   public set = (key: string, value: any) => {
     const keyHash = this.hash(key);
     this.keys[key] = keyHash;
     const bucketLinkedList = this.buckets[keyHash];
-    const node = bucketLinkedList.find({callback: nodeData => nodeData.key === key});
+    const node = bucketLinkedList.find({
+      callback: nodeData => nodeData.key === key
+    });
 
-    if(!node) {
-      bucketLinkedList.append({key, value});
-    }else{
-      node.data.value = value; 
+    if (!node) {
+      bucketLinkedList.append({ key, value });
+    } else {
+      node.data.value = value;
     }
-  }
+  };
 
   public delete = (key: string) => {
     const keyHash = this.keys[key];
     delete this.keys[key];
     const bucketLinkedList = this.buckets[keyHash];
-    const node = bucketLinkedList.find({callback: nodeData => nodeData.key === key});
+    const node = bucketLinkedList.find({
+      callback: nodeData => nodeData.key === key
+    });
 
-    if(node) {
+    if (node) {
       return bucketLinkedList.delete(node.data);
     }
 
     return null;
-  }
+  };
 
   public get = (key: string) => {
     const bucketLinkedList = this.buckets[this.hash(key)];
-    const node = bucketLinkedList.find({callback: nodeData => nodeData.key === key});
+    const node = bucketLinkedList.find({
+      callback: nodeData => nodeData.key === key
+    });
 
     return node ? node.data.value : undefined;
-  }
+  };
 
   public has = (key: string) => {
     return Object.hasOwnProperty.call(this.keys, key);
-  }
+  };
 
   public getKeys = () => {
     return Object.keys(this.keys);
-  }
+  };
 }
